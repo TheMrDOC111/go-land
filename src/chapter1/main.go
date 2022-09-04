6f8d4981f8994989e6ed50c01c62b818a77312a2
DOC <themrdoc1111@gmail.com>
2022-09-04 22:50:46 +0300

    init project

---
+++ src/chapter1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"awesomeProject/src/chapter1/other"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(other.Hero)
+}
---
+++ src/chapter1/other/utils.go
@@ -0,0 +1,3 @@
+package other
+
+var Hero string = "hello,World"
